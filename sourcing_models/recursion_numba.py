99783ea99c65cd9947aaee8a0646d8f32e49945d
Ioannis Fragkos <ioannis.fragkos@gmail.com>
2021-06-09 14:02:02 +0300

    Improved numba version and base recursion.py

--- sourcing_models/recursion_numba.py
+++ sourcing_models/recursion_numba.py
@@ -5,10 +5,46 @@ from sys import argv
 from itertools import product
 from numba import njit
 from numba import types
-from numba.typed import Dict
+from numba.typed import Dict, List
+
+data = namedtuple('data', 'c_e c_r l_e l_r h b demand')
+demand = namedtuple('demand', 'min max support')
+
+
+def load_data():
+    """
+    Sample file structure:
+    100 (c_e)
+    0   (c_r)
+    0   (le)
+    2   (lr)
+    5   (h)
+    95  (b)
+    0   (min demand)
+    4   (max demand)
+    """
+    with open(filename, 'r') as f:
+        data.c_e = float(f.readline())
+        data.c_r = float(f.readline())
+        data.l_e = float(f.readline())
+        data.l_r = float(f.readline())
+        data.h = float(f.readline())
+        data.b = float(f.readline())
+        d_min = float(f.readline())
+        d_max = float(f.readline())
+        # demand can be modeled in a better way (needs to be a class)
+        # I will keep it like this for now
+        demand.min = d_min
+        demand.max = d_max
+        support = d_max - d_min
+        demand.support = support
+        demand.prob = dict(zip(np.arange(d_min, d_max + 1), np.repeat(1 / (support + 1), support + 1)))
+    data.demand = demand
+    return data
+
 
 @njit
-def vf_update(state, vf, demand_prob, actions, states):
+def vf_update(state, vf, demand_prob, actions):
     """
     Calculation of value iteration for a single update.
     state:   tuple of integers of length l=lf-le: [IPe, qr(t-l+1), ..., qr(t-1)]
@@ -26,23 +62,20 @@ def vf_update(state, vf, demand_prob, actions, states):
         # Partial state update
         ip_e = state[0] + qe + state[1]
 
-        if state[2:]:
+        if lr > 2:
             pipeline = state[2:]
-        else: 
+        else:
             pipeline = qr
 
         for dem in range(d_min, d_max + 1):
             ipe_new = ip_e - dem
-            # This works only for l = 2, need to work out the general case
-            # this_state = (ipe_new, qr)
-            # This should work for the general case
-            if state[2:]:
-                pass # this_state = (ipe_new, pipeline[0], qr) 
+            if lr > 2:
+                this_state = (ipe_new, *pipeline, qr)
             else:
                 this_state = (ipe_new, qr)
             # If we jump to a state that is not in our list, we are not playing optimal
             # so we can safely get out of here. 
-            if this_state not in states:
+            if this_state not in vf:
                 cost = 10e9
                 break
             else:
@@ -54,13 +87,8 @@ def vf_update(state, vf, demand_prob, actions, states):
             best_cost = cost
             best_action = (qe, qr)
 
-    #if not best_action:
-        # If there is no best action it means we were left in a state we were not supposed 
-        # to ever get there with optimal play; we can remove it
-        #vf.pop(state, None)
-        #states.remove(state)
-
     return best_cost, best_action
+
 
 def main():
     """
@@ -70,67 +98,72 @@ def main():
     filename (str): filename of parameter file.
     
     """
-    # In problems where demand in [0, 4], the expedited inventory position is between -8 and 13
     # Note that some of the states should never be reached (the ones with high inventory and high qr)
-    # If we land in such a state we will remove it
+    # If we land in such a state we will ignore it
     dim_pipeline = lr - le - 1
-    states = list(product(range(-8, 15 + 1), *(range(5 + 1),) * int(dim_pipeline)))
+    min_ip = int(d_max * lr)
+    max_ip = int((lr + 1) * (d_max + 1) + d_max)
+    states_ = list(product(range(-min_ip, max_ip + 1), *(range(5 + 1),) * int(dim_pipeline)))
+    states = List()
+    for state in states_:
+        states.append(state)
     # SW mention we never need to order more than max demand for any mode
-    actions = list(product(range(5+1), range(5+1)))
+    actions_ = list(product(range(5 + 1), range(5 + 1)))
+    actions = List()
+    for action in actions_:
+        actions.append(action)
     # Values can be initiated arbitrarily
     vals = np.repeat(1., len(states))
-    vf_ = dict(zip(states,vals))
-    
-    vf = Dict.empty(key_type=types.UniTuple(types.int64, 2),value_type=types.float64)       
-    for k, v in vf_.items():
-        vf[k] = v 
-        
-    demand_prob_ = dict(zip(np.arange(d_min, d_max + 1), np.repeat(1 / (support+1), support+1)))
 
-    demand_prob = Dict.empty(key_type=types.float64,value_type=types.float64)       
+    vf_ = dict(zip(states, vals))
+
+    vf = Dict.empty(key_type=types.UniTuple(types.int64, lr), value_type=types.float64)
+    for k, v in vf_.items():
+        vf[k] = v
+
+    demand_prob_ = demand.prob
+
+    demand_prob = Dict.empty(key_type=types.float64, value_type=types.float64)
     for k, v in demand_prob_.items():
         demand_prob[k] = v
-    
-    max_iterations, tolerance, delta = 200000, 10e-8, 10.
+
+    max_iterations, tolerance, delta = 200000, 10e-9, 10.
     all_values = np.zeros(max_iterations)
     these_values = np.zeros(len(states))
 
     start_time = time.time()
 
     # Main value iteration loop
-    for iteration in range(5000):
+    for iteration in range(max_iterations):
         # We first store each newly updated state
-        
+
         for idx, state in enumerate(states):
-            #print(idx, state, len(states))
-            these_values[idx], best_action = vf_update(state, vf, demand_prob, actions, states)
+            # print(idx, state, len(states), vf_update(state, vf, demand_prob, actions, states)[0])
+            these_values[idx] = vf_update(state, vf, demand_prob, actions)[0]
         # After the minimum for each state has been calculated, we update the states
         # If done in the same loop, converge sucks even more
         for idx, state in enumerate(states):
             vf[state] = these_values[idx]
 
         this_average = np.mean([val for val in vf.values() if val < 10e8])
-        
-        all_values[iteration] = this_average/(iteration+1)
+
+        all_values[iteration] = this_average / (iteration + 1)
 
         if iteration > 1 and iteration % 100 == 0:
-            print('iteration: %d average cost: %1.2f'%(iteration,all_values[iteration]))
+            print('iteration: %d average cost: %1.2f' % (iteration, all_values[iteration]))
             delta = all_values[iteration - 1] - all_values[iteration]
             if delta <= tolerance:
                 break
 
     end_time = time.time() - start_time
-    print('DP terminated after %1.2f seconds. Tolerance: %1.9f Iterations: %d'%(end_time,delta,iteration))
+    print('DP terminated after %1.2f seconds. Tolerance: %1.9f Iterations: %d' % (end_time, delta, iteration))
 
 
 if __name__ == '__main__':
-   ce = 20
-   cr = 0
-   le = 0
-   lr = 2
-   h = 5
-   b = 495
-   d_min = 0
-   d_max = 4
-   support = d_max - d_min
-   main()
+    filename = 'ds1.in' if not len(argv) > 1 else argv[1]
+    # filename = '..\..\Instances\ce20lr_3_b495.in'
+    instance_data = load_data()
+    ce, cr, lr, le = instance_data.c_e, instance_data.c_r, instance_data.l_r, instance_data.l_e
+    d_min, d_max, demand = instance_data.demand.min, instance_data.demand.max, instance_data.demand
+    h, b = instance_data.h, instance_data.b
+    main()
