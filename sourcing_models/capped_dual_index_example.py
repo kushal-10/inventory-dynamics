9de89cf7390a0b092fb68bd64085ed08e507c98c
Lucas Boettcher <l.boettcher@fs.de>
2021-08-05 11:36:45 +0200

    code updates

--- sourcing_models/capped_dual_index_example.py
+++ sourcing_models/capped_dual_index_example.py
@@ -4,31 +4,31 @@ from lib.dual_sourcing import *
 
 #np.random.seed(10)
 
-ce = 20
+ce = 10
 cr = 0
 le = 0
-lr = 2
+lr = 4
 h = 5
 b = 495
 
-T = 30000
+# T = 30000
 
-u1_arr = np.arange(1,5)
-u2_arr = np.arange(8,12)
-u3_arr = np.arange(5)
+# u1_arr = np.arange(1,5)
+# u2_arr = np.arange(8,14)
+# u3_arr = np.arange(5)
 
-optimal_u1, optimal_u2, optimal_u3 = capped_dual_index_parameters(u1_arr,
-                                                                  u2_arr,
-                                                                  u3_arr,
-                                                                  ce, 
-                                                                  cr, 
-                                                                  le, 
-                                                                  lr,
-                                                                  h, 
-                                                                  b, 
-                                                                  T)
+# optimal_u1, optimal_u2, optimal_u3 = capped_dual_index_parameters(u1_arr,
+#                                                                    u2_arr,
+#                                                                    u3_arr,
+#                                                                    ce, 
+#                                                                    cr, 
+#                                                                    le, 
+#                                                                    lr,
+#                                                                    h, 
+#                                                                    b, 
+#                                                                    T)
 
-T = 100
+T = 500000
 S = DualSourcingModel(ce=ce, 
                       cr=cr, 
                       le=le, 
@@ -36,10 +36,10 @@ S = DualSourcingModel(ce=ce,
                       h=h, 
                       b=b,
                       T=T, 
-                      I0=0,
+                      I0=4,
                       u1=4,
-                      u2=9,
-                      u3=3,
+                      u2=12,
+                      u3=2,
                       capped_dual_index=True)
 
 S.simulate()  
@@ -55,29 +55,29 @@ S.simulate()
 
 print("average cost (capped dual index):", S.total_cost/T)
 
-plt.figure()
-plt.plot(S.inventory, '-o', label = r"inventory")
-plt.plot(S.inventory_position, '-o', label = r"inventory position")
-plt.plot(S.demand, '-o', label = r"demand")
-plt.xlabel(r"time")
-plt.ylabel(r"value")
-plt.legend(loc = 4, ncol = 3)
-plt.tight_layout()
-plt.show()
+# plt.figure()
+# plt.plot(S.inventory, '-o', label = r"inventory")
+# plt.plot(S.inventory_position, '-o', label = r"inventory position")
+# plt.plot(S.demand, '-o', label = r"demand")
+# plt.xlabel(r"time")
+# plt.ylabel(r"value")
+# plt.legend(loc = 4, ncol = 3)
+# plt.tight_layout()
+# plt.show()
 
-plt.figure()
-plt.plot(S.qe, '-o', label = r"expedited order")
-plt.plot(S.qr, '-o', label = r"regular order")
-plt.xlabel(r"time")
-plt.ylabel(r"value")
-plt.legend(loc = 4, ncol = 3)
-plt.tight_layout()
-plt.show()
+# plt.figure()
+# plt.plot(S.qe, '-o', label = r"expedited order")
+# plt.plot(S.qr, '-o', label = r"regular order")
+# plt.xlabel(r"time")
+# plt.ylabel(r"value")
+# plt.legend(loc = 4, ncol = 3)
+# plt.tight_layout()
+# plt.show()
 
-plt.figure()
-plt.plot(S.cost, '-o', label = r"cost")
-plt.xlabel(r"time")
-plt.ylabel(r"cost")
-plt.legend(loc = 4, ncol = 3)
-plt.tight_layout()
-plt.show()
+# plt.figure()
+# plt.plot(S.cost, '-o', label = r"cost")
+# plt.xlabel(r"time")
+# plt.ylabel(r"cost")
+# plt.legend(loc = 4, ncol = 3)
+# plt.tight_layout()
+# plt.show()
