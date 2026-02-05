05dfc5c423ee06ccbe63b76b9a0c9138fbe9433e
kushal-10 <kushalkoshti11@gmail.com>
2026-02-05 14:34:34 +0100

    optimized NN controller

--- src/idinn/periodic_controller/results/train_multi_headed_nn.py
+++ src/idinn/periodic_controller/results/train_multi_headed_nn.py
@@ -32,7 +32,11 @@ sourcing_model = DualSourcingModel(
 def train_best_model():
     
     controller = MultiHeadedNeuralController(
+<<<<<<< HEAD
         shared_layers=[32, 16],
+=======
+        shared_layers=[64, 32, 16],
+>>>>>>> feat/nn_controllers
         head_even_layers=[8,4],
         head_odd_layers=[4]
     )
