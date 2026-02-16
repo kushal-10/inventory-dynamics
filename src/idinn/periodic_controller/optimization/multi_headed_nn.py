b61c1135938abc8c15667ccc4ef12968e9134277
kushal-10 <kushalkoshti11@gmail.com>
2026-02-16 12:57:22 +0100

    add n_cycles

--- src/idinn/periodic_controller/optimization/multi_headed_nn.py
+++ src/idinn/periodic_controller/optimization/multi_headed_nn.py
@@ -36,8 +36,8 @@ def objective(trial):
             ],
         )
 
-        head_even_layers = trial.suggest_categorical(
-            "head_even_layers",
+        head_restricted_layers = trial.suggest_categorical(
+            "head_restricted_layers",
             [
                 [8, 4],
                 [4],
@@ -45,8 +45,8 @@ def objective(trial):
             ],
         )
 
-        head_odd_layers = trial.suggest_categorical(
-            "head_odd_layers",
+        head_regular_layers = trial.suggest_categorical(
+            "head_regular_layers",
             [
                 [8, 4],
                 [4],
@@ -57,29 +57,29 @@ def objective(trial):
         MAX_Q = trial.suggest_categorical(
             "MAX_Q",
             [
-                i for i in range(17,24)
+                i for i in range(10,30)
             ]
         )
 
         parameters_lr_shared = trial.suggest_loguniform("parameters_lr_shared", 1e-4, 1e-2)
-        parameters_lr_even = trial.suggest_loguniform("parameters_lr_even", 1e-4, 1e-2)
-        parameters_lr_odd = trial.suggest_loguniform("parameters_lr_odd", 1e-4, 1e-2)
+        parameters_lr_restricted = trial.suggest_loguniform("parameters_lr_restricted", 1e-4, 1e-2)
+        parameters_lr_regular = trial.suggest_loguniform("parameters_lr_regular", 1e-4, 1e-2)
 
         weight_decay_shared = trial.suggest_loguniform("weight_decay_shared", 1e-6, 1e-3)
-        weight_decay_even = trial.suggest_loguniform("weight_decay_even", 1e-6, 1e-3)
-        weight_decay_odd = trial.suggest_loguniform("weight_decay_odd", 1e-6, 1e-3)
+        weight_decay_restricted = trial.suggest_loguniform("weight_decay_restricted", 1e-6, 1e-3)
+        weight_decay_regular = trial.suggest_loguniform("weight_decay_regular", 1e-6, 1e-3)
 
 
         mlflow.log_params({
             "shared_layers": str(shared_layers),
-            "head_even_layers": str(head_even_layers),
-            "head_odd_layers": str(head_odd_layers),
+            "head_regular_layers": str(head_regular_layers),
+            "head_restricted_layers": str(head_restricted_layers),
             "parameters_lr_shared": parameters_lr_shared,
             "weight_decay_shared": weight_decay_shared,
-            "parameters_lr_odd": parameters_lr_odd,
-            "parameters_lr_even": parameters_lr_even,
-            "weight_decay_even": weight_decay_even,
-            "weight_decay_odd": weight_decay_odd,
+            "parameters_lr_restricted": parameters_lr_restricted,
+            "parameters_lr_regular": parameters_lr_regular,
+            "weight_decay_restricted": weight_decay_restricted,
+            "weight_decay_regular": weight_decay_regular,
             "MAX_Q": MAX_Q
         })
 
@@ -97,8 +97,8 @@ def objective(trial):
 
         controller = MultiHeadedNeuralController(
             shared_layers=shared_layers,
-            head_even_layers=head_even_layers,
-            head_odd_layers=head_odd_layers,
+            head_regular_layers=head_regular_layers,
+            head_restricted_layers=head_restricted_layers,
             MAX_Q=MAX_Q
         )
 
@@ -107,11 +107,11 @@ def objective(trial):
             sourcing_periods=100,
             epochs=600,
             weight_decay_shared = weight_decay_shared,
-            weight_decay_odd = weight_decay_odd,
-            weight_decay_even = weight_decay_even,
+            weight_decay_restricted = weight_decay_restricted,
+            weight_decay_regular = weight_decay_regular,
             parameters_lr_shared = parameters_lr_shared,
-            parameters_lr_odd = parameters_lr_odd,
-            parameters_lr_even = parameters_lr_even,
+            parameters_lr_restricted = parameters_lr_restricted,
+            parameters_lr_regular = parameters_lr_regular,
             seed=42,
         )
 
@@ -145,18 +145,3 @@ if __name__ == '__main__':
     best_params = run_hpo()
     logger.info(f"Best Parameters: {best_params}")
 
-
-"""
-[I 2026-02-05 13:27:49,383] Trial 13 finished with value: 26.93000030517578 and parameters: {'shared_layers': [64, 32, 16], 
-'head_even_layers': [8, 4], 
-'head_odd_layers': [4], 
-'MAX_Q': 20, 
-'parameters_lr_shared': 0.004284520099077406, 
-'parameters_lr_even': 0.000372801186818003, 
-'parameters_lr_odd': 0.00034988574403358624, 
-'weight_decay_shared': 0.000282468537229603, 
-'weight_decay_even': 1.1267508218370485e-06, 
-'weight_decay_odd': 5.343701588766947e-06
-
-}. Best is trial 13 with value: 26.93000030517578.
-"""
